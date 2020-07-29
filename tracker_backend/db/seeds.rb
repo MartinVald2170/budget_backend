a5b8d6c4bfdb933d89d0dd6c139dffee114b481f
MartinVald2170 <noways2170@gmail.com>
2020-07-29 12:13:03 +1000

    ready to deploy

---
+++ tracker_backend/db/seeds.rb
@@ -0,0 +1,9 @@
+for i in 1..10 
+    Expense.create(
+        title: Faker::JapaneseMedia::OnePiece.character,
+        value: Faker::Types.rb_integer,
+        due: Faker::Date.between(from: "2020-08-01", to: "2021-01-01")
+    )
+
+    puts "Created #{i} expenses"
+end
